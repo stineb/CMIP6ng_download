9ec06499fb9cdef7d2cad3586c8dfd8c2fdcef87
Beni Stocker <benjamin.stocker@gmail.com>
2024-05-04 21:02:15 +0200

    updated download list

--- download_cmip6.sh
+++ download_cmip6.sh
@@ -7,11 +7,19 @@ fi
 
 # select the required files (append to file)
 # stack grep commands to filter to the desired level
+
+# evapotranspiration, monthly (mm s-1)
+cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "evspsbl_" | grep "mon" cut -d' ' -f5 > download_list.txt
+
+# precipitation, daily
 cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "pr_day" | cut -d' ' -f5 > download_list.txt
+
+# temperature, daily
 cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "tas_day" | cut -d' ' -f5 >> download_list.txt
-cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "rlds_mon" | cut -d' ' -f8 >> download_list.txt
-cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "rtmt_mon" | cut -d' ' -f8 >> download_list.tx
-cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "tran_mon" | cut -d' ' -f8 >> download_list.txt
+
+# cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "rlds_mon" | cut -d' ' -f8 >> download_list.txt
+# cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "rtmt_mon" | cut -d' ' -f8 >> download_list.tx
+# cat /tmp/cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "tran_mon" | cut -d' ' -f8 >> download_list.txt
 
 cat download_list.txt
 
